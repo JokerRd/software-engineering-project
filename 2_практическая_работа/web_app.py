1658fb2269eb8c8d3fb0462f71166ec9c8bb53cc
Vlad <v.a.konkob@mail>
2023-11-07 23:27:20 +0500

    Реализовал функцию для сравнения двух текстов

--- 2_практическая_работа/web_app.py
+++ 2_практическая_работа/web_app.py
@@ -1,7 +1,26 @@
 import streamlit as st
-import pandas as pd
+from sentence_transformers import SentenceTransformer, util
+import re
 
-st.write("""
-# My first app
-Hello *world!*
-""")
+
+def calculate_average_score(first_text: str, second_text: str, separators: list):
+    if first_text == '' or second_text == '':
+        return 0
+    regex_separators = '[' + ''.join(separators) + ']'
+
+    first_text_sentences = re.split(regex_separators, first_text)
+    second_text_sentences = re.split(regex_separators, second_text)
+    length_row = len(first_text_sentences)
+
+    model = SentenceTransformer('sentence-transformers/LaBSE')
+
+    first_embeddings = model.encode(first_text_sentences)
+    second_embeddings = model.encode(second_text_sentences)
+
+    cos_sim_arr = util.cos_sim(first_embeddings, second_embeddings)
+
+    total_score = sum(map(lambda row: max(row), cos_sim_arr))
+    average_score = total_score / length_row
+    return average_score
+
+
