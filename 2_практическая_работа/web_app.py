1ef7f85d1194582d1f49b2bbe5e2e34b6d636fd2
Никита <65447961+S1ide@users.noreply.github.com>
2023-11-07 23:35:39 +0500

    Update web_app.py

--- 2_практическая_работа/web_app.py
+++ 2_практическая_работа/web_app.py
@@ -22,5 +22,51 @@ def calculate_average_score(first_text: str, second_text: str, separators: list)
     total_score = sum(map(lambda row: max(row), cos_sim_arr))
     average_score = total_score / length_row
     return average_score
+    
+def compare_text(first_text: str, second_text: str, options: list):
+    separators = list(map(lambda option: option_to_separator(option), options))
+    average_score = calculate_average_score(first_text, second_text, separators)
+    average_score_percent = average_score * 100
 
+    st.text("Тексты похожи на {:.2f} %".format(average_score_percent))
+
+
+def option_to_separator(option: str):
+    if option == 'Точка':
+        return '.'
+    if option == 'Вопросительный знак':
+        return '?'
+    if option == 'Восклицательный знак':
+        return '!'
+    if option == 'Точка с запятой':
+        return ';'
+    if option == 'Запятая':
+        return ','
+    if option == 'Тире':
+        return '-'
+    return None
+
+
+st.title('Антиплагиат')
+
+st.write('Это приложение позволяет сравнить два текста между собой по предложениям и определяет '
+        'похожесть текстов друга на друга.')
+st.write('Для работы выберите какими разделителями разделены предложения, введите'
+        'последовательно два текста, которые хотите сравнить в соответствующие окна и нажмите кнопку сравнить. '
+        'Будет отображаться процент похожести текстов друг на друга.')
+
+options = st.multiselect(
+    'Выберите разделитель предложений',
+    ['Точка', 'Вопросительный знак', 'Восклицательный знак', 'Точка с запятой', 'Запятая', 'Тире'],
+    ['Точка', 'Вопросительный знак', 'Восклицательный знак'])
+
+col1, col2 = st.columns(2)
+
+with col1:
+    first_text_area = st.text_area('Введите первый текст для сравнения')
+
+with col2:
+    second_text_area = st.text_area('Введите второй текст для сравнения')
+
+button = st.button("Сравнить", on_click=compare_text(first_text_area, second_text_area, options))
 
