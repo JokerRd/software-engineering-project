1481f057cefb2501fe904b5726564272908fb980
Vlad <v.a.konkob@mail>
2023-12-14 18:09:34 +0500

    Практическая работа 3. Добавил api + model

---
+++ 3_практическая_работа/api.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from pydantic import BaseModel, Field
+from starlette import status
+from starlette.responses import JSONResponse
+
+from model import get_stat_by_sentences
+
+from model import calculate_average_score
+
+app = FastAPI()
+
+
+class CompareTextRequest(BaseModel):
+    first_text: str = Field(min_length=1)
+    second_text: str = Field(min_length=1)
+    separators: list[str] = Field(min_items=1, default=['.', '!', '?'])
+    is_percent: bool = Field(default=False)
+
+
+class ShortResult(BaseModel):
+    similarity: float | str
+
+
+@app.post("/model/text/analysis/short", summary="Метод расчитывает сходство двух текстов")
+def calculate_text_similarity(compare_text: CompareTextRequest):
+    similarity_tensor = calculate_average_score(compare_text.first_text,
+                                                compare_text.second_text,
+                                                compare_text.separators)
+    similarity = float(similarity_tensor)
+    if compare_text.is_percent:
+        return ShortResult(similarity=convert_to_percent(similarity))
+    return ShortResult(similarity=similarity)
+
+
+@app.post("/model/text/analysis/full", summary="Метод расчитывает сходство предложений двух "
+                                      "текстов по принципу каждое с каждым")
+def get_full_stat_compare_text(compare_text: CompareTextRequest):
+    stats = get_stat_by_sentences(compare_text.first_text, compare_text.second_text, compare_text.separators)
+    if compare_text.is_percent:
+        return list(map(lambda item: modify_stat(item), stats))
+    return stats
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    custom = list(map(lambda item: {"field": item['loc'][-1], "message": item['msg']}, exc.errors()))
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"validation_errors": custom, "body": exc.body}),
+    )
+
+
+def modify_stat(stat):
+    stat['similarity'] = convert_to_percent(stat['similarity'])
+    return stat
+
+
+def convert_to_percent(similarity: float):
+    rounded_percent_similarity = round(similarity * 100, 2)
+    return f"{rounded_percent_similarity}%"
---
+++ 3_практическая_работа/model.py
@@ -0,0 +1,53 @@
+from sentence_transformers import SentenceTransformer, util
+import re
+
+
+def calculate_average_score(first_text: str, second_text: str, separators: list):
+    cos_sim_arr, first_text_sentences, second_text_sentences = compare_text(first_text, second_text, separators)
+    length_row = len(first_text_sentences)
+    total_score = sum(map(lambda row: max(row), cos_sim_arr))
+    average_score = total_score / length_row
+    return average_score
+
+
+def get_stat_by_sentences(first_text: str, second_text: str, separators: list):
+    cos_sim_arr, first_text_sentences, second_text_sentences = compare_text(first_text, second_text, separators)
+    result = []
+    for i in range(len(cos_sim_arr)):
+        for j in range(len(cos_sim_arr[i])):
+            result.append({
+                "first_sentence": first_text_sentences[i],
+                "second_sentence": second_text_sentences[j],
+                "similarity": float(cos_sim_arr[i][j])
+            })
+
+    return result
+
+
+def compare_text(first_text: str, second_text: str, separators: list):
+    if first_text == '' or second_text == '':
+        return 0
+    regex_separators = '[' + ''.join(separators) + ']'
+
+    first_text_sentences_raw = re.split(regex_separators, first_text)
+    second_text_sentences_raw = re.split(regex_separators, second_text)
+    first_text_sentences = trim_spaces(filter_empty_sentences(first_text_sentences_raw))
+    second_text_sentences = trim_spaces(filter_empty_sentences(second_text_sentences_raw))
+
+    model = SentenceTransformer('sentence-transformers/LaBSE')
+
+    first_embeddings = model.encode(first_text_sentences)
+    second_embeddings = model.encode(second_text_sentences)
+
+    cos_sim_arr = util.cos_sim(first_embeddings, second_embeddings)
+    return cos_sim_arr, first_text_sentences, second_text_sentences
+
+
+def filter_empty_sentences(sentences: list[str]):
+    return list(filter(lambda item: item != '', sentences))
+
+
+def trim_spaces(sentences: list[str]):
+    return list(map(lambda item: item.strip(), sentences))
+
+
